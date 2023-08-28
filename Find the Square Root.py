f18eda638b6360baf7bdaf8657936a70f7a73723
Oli Perroni <134059994+oliperroni@users.noreply.github.com>
2023-08-28 12:10:02 +0300

    Update Find the Square Root.py

--- Find the Square Root.py
+++ Find the Square Root.py
@@ -1,3 +1,4 @@
+# Importing the complex math module
 import cmath
 
 num = 8 
@@ -8,13 +9,6 @@ num = 8
 num_sqrt = num ** 0.5
 print('The square root of %0.3f is %0.3f'%(num ,num_sqrt))
 
-# Importing the complex math module
-
-
 num = 1+2j
-
-# To take input from the user
-#num = eval(input('Enter a number: '))
-
 num_sqrt = cmath.sqrt(num)
 print('The square root of {0} is {1:0.3f}+{2:0.3f}j'.format(num ,num_sqrt.real,num_sqrt.imag))
