9a30a3a7c362a1acc84d913c6b96bae998164be7
Oli Perroni <134059994+oliperroni@users.noreply.github.com>
2023-09-23 07:00:31 +0300

    Update calculate_the_area_of_a_triangle.py

--- calculate_the_area_of_a_triangle.py
+++ calculate_the_area_of_a_triangle.py
@@ -11,6 +11,6 @@ c = 16
 # Calculate the semi-perimeter
 s = (a + b + c) / 2
 
-# calculate the area
-area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
+# Calculate the area
+area = (s * (s-a)*(s-b)*(s-c)) ** 0.5
 print('The area of the triangle is %0.2f' %area)
