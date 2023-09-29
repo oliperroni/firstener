16fbbfc035fe9a83ed532cc30237e537bb289961
Oli Perroni <134059994+oliperroni@users.noreply.github.com>
2023-09-29 06:56:39 +0300

    Update calculate_the_area_of_a_triangle.py

--- calculate_the_area_of_a_triangle.py
+++ calculate_the_area_of_a_triangle.py
@@ -12,5 +12,6 @@ c = 16
 s = (a + b + c) / 2
 
 # Calculate the area
-area = (s * ( s - a ) * ( s - b ) * ( s -c )) ** 0.5
+area = (s * (s - a) * (s - b) * (s - c)) ** 0.5
+
 print('The area of the triangle is %0.2f' %area)
