414ce1ceff683911e310dc131c809bd0d8039d3e
Oli Perroni <134059994+oliperroni@users.noreply.github.com>
2023-11-03 09:43:03 +0300

    Update solve_quadratic_equation.py

--- solve_quadratic_equation.py
+++ solve_quadratic_equation.py
@@ -14,4 +14,4 @@ d = (b ** 2) - (4 * a * c)
 sol1 = (-b - cmath.sqrt(d)) / (2 * a)
 sol2 = (-b + cmath.sqrt(d))/(2 * a)
 
-print('The solution are {sol1} and {sol2}'.format(sol1,sol2))
+print(f'The solution are {sol1} and {sol2}')
