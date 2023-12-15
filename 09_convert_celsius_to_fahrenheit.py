ba0fb228257e40e98439cc652b913d88e125da5e
Oli Perroni <134059994+oliperroni@users.noreply.github.com>
2023-12-15 09:13:37 +0300

    Update and rename 09_Convert Celsius To Fahrenheit to 09_convert_celsius_to_fahrenheit.py

--- 09_convert_celsius_to_fahrenheit.py
+++ 09_convert_celsius_to_fahrenheit.py
@@ -5,4 +5,4 @@ celsius = 37.5
 
 # calculate fahrenheit
 fahrenheit = (celsius * 1.8) + 32
-print(f'{celsius} degree Celsius is equal to %0.1f degree Fahrenheit' %(,fahrenheit))
+print(f'{celsius} degree Celsius is equal to {fahrenheit} degree Fahrenheit')
