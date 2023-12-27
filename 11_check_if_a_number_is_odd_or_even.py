826d1740da82e8e582b42854ed0e499a20e48bca
Oli Perroni <134059994+oliperroni@users.noreply.github.com>
2023-12-27 08:53:26 +0300

    Update 11_check_if_a_number_is_odd_or_even.py

--- 11_check_if_a_number_is_odd_or_even.py
+++ 11_check_if_a_number_is_odd_or_even.py
@@ -2,8 +2,8 @@
 # A number is even if division by 2 gives a remainder of 0.
 # If the remainder is 1, it is an odd number.
 
-num = int(input("Enter a number: "))
+num = int(input("enter a number: "))
 if (num % 2) == 0:
-   print(f"{num} is Even")
+   print(f"{num} is even")
 else:
-   print("{0} is Odd".format(num))
+   print("{0} is odd".format(num))
