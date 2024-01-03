7b6dba6984cdfcb618782e8c93ed1550bdc84d1a
Oli Perroni <134059994+oliperroni@users.noreply.github.com>
2024-01-03 11:44:45 +0300

    Update 12 Check Leap Year.py

--- 12 Check Leap Year.py
+++ 12 Check Leap Year.py
@@ -8,7 +8,7 @@ year = 2020
 # divided by 100 means century year (ending with 00)
 # century year divided by 400 is leap year
 if (year % 400 == 0) and (year % 100 == 0):
-    print("{0} is a leap year".format(year))
+    print(f"{year} is a leap year")
 
 # not divided by 100 means not a century year
 # year divided by 4 is a leap year
