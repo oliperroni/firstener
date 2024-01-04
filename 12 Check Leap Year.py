34d213399a2c57c2ba42b14f3eb9219e6bc39dcf
Oli Perroni <134059994+oliperroni@users.noreply.github.com>
2024-01-04 10:02:24 +0300

    Update 12 Check Leap Year.py

--- 12 Check Leap Year.py
+++ 12 Check Leap Year.py
@@ -13,7 +13,7 @@ if (year % 400 == 0) and (year % 100 == 0):
 # not divided by 100 means not a century year
 # year divided by 4 is a leap year
 elif (year % 4 ==0) and (year % 100 != 0):
-    print("{0} is a leap year".format(year))
+    print(f"{year} is a leap year")
 
 # if not divided by both 400 (century year) and 4 (not century year)
 # year is not leap year
