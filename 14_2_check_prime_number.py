fda2ee2bea5a71547aeb16652cc7e4b9a187f604
Oli Perroni <134059994+oliperroni@users.noreply.github.com>
2024-04-30 14:02:25 +0300

    Update 14_2_check_prime_number.py

--- 14_2_check_prime_number.py
+++ 14_2_check_prime_number.py
@@ -10,7 +10,7 @@ elif num > 1:
    for i in range(2,num):
        if (num % i) == 0:
            print(num,'is not a prime number')
-           print(i,"times",num//i,"is",num)
+           print(i,'times',num//i,"is",num)
            break
    else:
        print(num,"is a prime number")
