975fccce512db37dc0a89083e94aed7d96753224
Oli Perroni <134059994+oliperroni@users.noreply.github.com>
2025-01-21 12:22:45 +0300

    Update 15_print_all_prime_numbers.py

--- 15_print_all_prime_numbers.py
+++ 15_print_all_prime_numbers.py
@@ -1,7 +1,7 @@
 # Python program to display all the prime numbers within an interval
 
-lower = 2000
-upper = 3000
+lower = 22000
+upper = 30000
 
 print('Prime numbers between', lower, 'and', upper, 'are:')
 
