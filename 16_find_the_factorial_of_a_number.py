dc417ddbee9c2225990c6d06273d8d3e03fbc9d9
Oli Perroni <134059994+oliperroni@users.noreply.github.com>
2025-07-28 21:14:44 +0300

    Update 16_find_the_factorial_of_a_number.py

--- 16_find_the_factorial_of_a_number.py
+++ 16_find_the_factorial_of_a_number.py
@@ -1,7 +1,7 @@
 # Python program to find the factorial of a number provided by the user.
 
 # change the value for a different result
-num = 177
+num = 178
 
 # To take input from the user
 #num = int(input("Enter a number: "))
@@ -12,7 +12,7 @@ factorial = 1
 if num < 0:
    print("Sorry, factorial does not exist for negative numbers")
 elif num == 0:
-   print("The factorial of 0 is 1")
+   print('The factorial of 0 is 1')
 else:
    for i in range(1,num + 1):
        factorial = factorial*i
