e6ea5e70527ef27dc8deece3fe86dcb37cc0cea3
Oli Perroni <134059994+oliperroni@users.noreply.github.com>
2025-08-25 08:48:33 +0300

    Update 18_print_the_fibonacci_sequence.py

--- 18_print_the_fibonacci_sequence.py
+++ 18_print_the_fibonacci_sequence.py
@@ -11,7 +11,7 @@ if nterms <= 0:
    print("Please enter a positive integer")
 # if there is only one term, return n1
 elif nterms == 1:
-   print("Fibonacci sequence upto",nterms,":")
+   print("Fibonacci Sequence upto",nterms,":")
    print(n1)
 # generate Fibonacci Sequence
 else:
